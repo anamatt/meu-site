de0070cd04709dad871b303a6efc54de9a3fd1c3
anamatt <56138092+anamatt@users.noreply.github.com>
2019-12-02 21:43:23 -0300

    Update blockchain.js

--- javascript/blockchain.js
+++ javascript/blockchain.js
@@ -4,34 +4,6 @@ ethereum.enable();
 var signatario = provedor.getSigner();
 var contrato = new ethers.Contract(enderecoContrato, abiContrato, signatario);
 
-function registrarMudancaStatus() {
-    var textoCampo = document.frmStatus.txtStatusPagamentoAluguel.value;
-    var caixaStatusTx = document.getElementById("caixaStatusTx");
-    if (textoCampo.length === 8) {
-        caixaStatusTx.innerHTML = "Enviando transação...";
-        contrato.mudaStatusPagamento(textoCampo)
-        .then( (transacao) => {
-            console.log("registrarMudancaStatus - Transacao ", transacao);   
-            caixaStatusTx.innerHTML = "Transação enviada. Aguardando processamento...";
-            transacao.wait()
-            .then( (resultado) => {
-                buscaStatusContrato();
-                caixaStatusTx.innerHTML = "Transação realizada.";
-            })        
-            .catch( (err) => {
-                console.error("registrarMudancaStatus - Aguardando tx ser minerada");
-                console.error(err);
-                caixaStatusTx.innerHTML = "Algo saiu errado: " + err.message;
-            })
-        })
-        .catch( (err) => {
-            console.error("registrarMudancaStatus");
-            console.error(err);
-            caixaStatusTx.innerHTML = "Algo saiu errado: " + err.message;
-        })
-    }
-}
-
 function buscaStatusContrato() {
     var status;
     var campoStatus = document.getElementById("campoStatus");     
@@ -84,26 +56,15 @@ function buscaValorParcelas() {
     });
 }
 
-function finalizarContrato () {
-    contrato.fimDoContrato()
-    caixaStatusTx.innerHTML = "Enviando solicitação...";
-    .then( (transacao) => {
-        console.log("registrarFimContrato - Transacao ", transacao);   
-        caixaStatusTx.innerHTML = "Transação enviada. Aguardando processamento...";
-        transacao.wait()
-            .then( (resultado) => {
-                caixaStatusTx.innerHTML = "Transação realizada.";
-            })        
-            .catch( (err) => {
-                console.error("registrarFimContrato - Aguardando tx ser minerada");
-                console.error(err);
-                caixaStatusTx.innerHTML = "Algo saiu errado: " + err.message;
-            })
-        })
-    .catch( (err) => {
-        console.error("registrarFimContrato");
-        console.error(err);
-        caixaStatusTx.innerHTML = "Algo saiu errado: " + err.message;
+function buscaProximaParcela() {
+    var proximaParcela;
+    var campoDataParcela = document.getElementById("campoDataParcela");     
+    contrato.dataProximaParcela()
+    .then( (resultado) => {
+        campoDataParcela.innerHTML = resultado;
     })
-    }
+    .catch( (err) => {
+        console.error(err);
+        campoDataParcela.innerHTML = err;
+    });
 }
